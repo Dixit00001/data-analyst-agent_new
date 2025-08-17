832c99cb704d2cd27c90a45c05a285c965efea5d
Dixit00001 <dixitsharma311@gmail.com>
2025-08-17 21:30:33 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://8e3b5f7bb72b.ngrok-free.app/api/"
+url = "https://31cec38f331c.ngrok-free.app/api/"
 files = {'questions.txt': open('questions.txt', 'rb')}
 response = requests.post(url, files=files)
 print(response.text)
